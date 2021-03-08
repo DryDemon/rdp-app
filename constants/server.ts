75f65b874f8edc08587bb01068b20e8580473e5f
jean chartier-Kastler <jeancroco@gmail.com>
2021-03-08 21:00:27 +0100

    update server const

--- constants/server.ts
+++ constants/server.ts
@@ -1,1 +1,2 @@
-export const SERVER_API_URL="http://192.168.1.13:3000/api";
+// export const SERVER_API_URL="http://192.168.1.13:3000/api";
+export const SERVER_API_URL="https://rdp-server-n552aam5d-drydemon.vercel.app/api";
