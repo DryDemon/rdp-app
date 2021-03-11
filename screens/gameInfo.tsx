5d2fc4d92068ab26ecdde6520ab3dc5c8e93d987
jean chartier-kastler <jean.chartierkastler@gmail.com>
2021-03-11 15:17:45 +0100

    scroll view corrected

--- screens/gameInfo.tsx
+++ screens/gameInfo.tsx
@@ -5,20 +5,20 @@ import { Alert, StyleSheet, ScrollView, Image } from "react-native";
 import { User } from "../src/interaces/interfacesUsers";
 
 import {
-    ProtectedHeader,
-    Text,
-    View,
-    TextInput,
-    Button,
-    TextMainTitle,
-    TextTitle,
-    TextLabel,
-    ViewContainer,
-    ViewCenter,
-    TextWarning,
-    LineBreak,
-    SmallLineBreak,
-    TextSubTitle,
+	ProtectedHeader,
+	Text,
+	View,
+	TextInput,
+	Button,
+	TextMainTitle,
+	TextTitle,
+	TextLabel,
+	ViewContainer,
+	ViewCenter,
+	TextWarning,
+	LineBreak,
+	SmallLineBreak,
+	TextSubTitle,
 } from "../components/Themed";
 import { ENVIRONEMENT } from "../constants/Environement";
 import { SERVER_API_URL, SERVER_LOGO_URL } from "../constants/Server";
@@ -27,180 +27,197 @@ import { validURL } from "../src/smallFuncts";
 import { FlatList } from "react-native-gesture-handler";
 
 async function getCurrentGame(joinCode: string, jwt: string) {
-    const rawResponse = await fetch(
-        `${SERVER_API_URL}/getcurrentgameinfo?jwt=${jwt}&joinCode=${joinCode}`
-    );
-    const content = await rawResponse.json();
+	const rawResponse = await fetch(
+		`${SERVER_API_URL}/getcurrentgameinfo?jwt=${jwt}&joinCode=${joinCode}`
+	);
+	const content = await rawResponse.json();
 
-    return content;
+	return content;
 }
 
 export default function GameInfo({ navigation }: any) {
-    // const { lastScreen } = route.params;, {route, navigation }: any
+	// const { lastScreen } = route.params;, {route, navigation }: any
 
-    const [jwt, setJwt] = useState<string>("");
-    const [user, setUser] = useState<User>();
-    const [joinCode, setJoinCode] = useState("");
-    const [game, setGame] = useState<GameSchema>()
+	const [jwt, setJwt] = useState<string>("");
+	const [user, setUser] = useState<User>();
+	const [joinCode, setJoinCode] = useState("");
+	const [game, setGame] = useState<GameSchema>();
 
-    const [logoUrl, setlogoUrl] = useState(SERVER_LOGO_URL);
+	const [logoUrl, setlogoUrl] = useState(SERVER_LOGO_URL);
 
-    async function loadGameData() {
+	async function loadGameData() {
+		if (jwt && joinCode) {
+			let content = await getCurrentGame(joinCode, jwt);
 
-        if (jwt && joinCode) {
+			if (content.success == 1) {
+				setGame(content.game);
+				AsyncStorage.setItem("@game", JSON.stringify(content.game));
+			} else {
+				navigation.navigate("Dashboard");
+			}
+		}
+	}
 
-            let content = await getCurrentGame(joinCode, jwt)
+	function checkIfConnected() {
+		if (!jwt || !user) {
+			navigation.navigate("Login");
+		}
+		if (!joinCode) {
+			navigation.navigate("Dashboard");
+		}
+	}
 
-            if (content.success == 1) {
-                setGame(content.game);
-                AsyncStorage.setItem("@game", JSON.stringify(content.game));
-            } else {
-                navigation.navigate("Dashboard");
-            }
+	//reload game data each time
+	useEffect(() => {
+		if (!game) loadGameData();
+	}, [joinCode, jwt]);
 
-        }
+	useEffect(() => {
+		if (!jwt || !user || !joinCode || !game) {
+			try {
+				if (!jwt) {
+					AsyncStorage.getItem("@jwt").then(
+						(value: string | null) => {
+							if (value) setJwt(value);
+						}
+					);
+				}
+				if (!user) {
+					AsyncStorage.getItem("@user").then(
+						(value: string | null) => {
+							if (value) setUser(JSON.parse(value));
+						}
+					);
+				}
+				if (!joinCode) {
+					AsyncStorage.getItem("@joinCode").then(
+						(value: string | null) => {
+							if (value) setJoinCode(value);
+						}
+					);
+				}
+				if (!game) {
+					//store it to get a better loading time
+					AsyncStorage.getItem("@game").then(
+						(value: string | null) => {
+							if (value != null) setGame(JSON.parse(value));
+						}
+					);
+					//and perform a in the use effect connected to jwt and joincode
+				}
+			} catch (e) {
+				if (ENVIRONEMENT == "dev") alert(e);
+			}
+		}
+	}, []);
 
-    }
+	useEffect(() => {
+		if (ENVIRONEMENT == "dev") console.log(game);
 
-    function checkIfConnected() {
-        if (!jwt || !user) {
-            navigation.navigate("Login");
-        }
-        if (!joinCode) {
-            navigation.navigate("Dashboard");
-        }
-    }
+		if (game && game.logoUrl && validURL(game.logoUrl))
+			setlogoUrl(game.logoUrl);
+	}, [game]);
 
+	const renderBulletList = (item: any, index: any) => (
+		<View style={styles.row} key={index}>
+			<View style={styles.bulletContainer}>
+				<View style={styles.bullet}>
+					<Text>{"\u2022" + " "}</Text>
+				</View>
+				<Text style={styles.bulletSubText}>{item.name}</Text>
+			</View>
+		</View>
+	);
+	return (
+		<View>
+			<ProtectedHeader back={"Dashboard"} navigation={navigation} />
+			<ViewContainer>
+				<SmallLineBreak />
+				<TextSubTitle style={styles.titleGame}>Info</TextSubTitle>
+				<ScrollView>
+					<View style={styles.textToMiddle}>
+						{game ? <TextTitle>{game?.name}</TextTitle> : undefined}
+						<SmallLineBreak />
 
-    //reload game data each time
-    useEffect(() => {
-        if (!game) loadGameData();
+						<Image
+							style={styles.gameLogo}
+							source={{
+								uri: logoUrl,
+							}}
+						/>
+						<SmallLineBreak />
 
-    }, [joinCode, jwt])
+						<TextSubTitle>Compétitions : </TextSubTitle>
+						<SmallLineBreak />
 
-    useEffect(() => {
+						<View style={styles.column}>
+							{game?.leagueIdList.map(
+								(item: any, index: number) => {
+									console.log(item, index);
+									return renderBulletList(item, index);
+								}
+								//
+							)}
+						</View>
 
-        if (!jwt || !user || !joinCode || !game) {
-            try {
-                if (!jwt) {
-                    AsyncStorage.getItem("@jwt").then((value: string | null) => {
-                        if (value) setJwt(value);
-                    });
-                }
-                if (!user) {
-                    AsyncStorage.getItem("@user").then((value: string | null) => {
-                        if (value) setUser(JSON.parse(value));
-                    });
-                }
-                if (!joinCode) {
-                    AsyncStorage.getItem("@joinCode").then((value: string | null) => {
-                        if (value) setJoinCode(value);
-                    });
-                }
-                if (!game) {
-                    //store it to get a better loading time
-                    AsyncStorage.getItem("@game").then((value: string | null) => {
-                        if (value != null) setGame(JSON.parse(value));
-                    });
-                    //and perform a in the use effect connected to jwt and joincode
-                }
-            } catch (e) {
-                if (ENVIRONEMENT == "dev") alert(e);
-            }
-        }
-    }, [])
+						<SmallLineBreak />
+						<TextSubTitle>Code du Contest : </TextSubTitle>
+						<SmallLineBreak />
+					</View>
 
-    useEffect(() => {
-        if (ENVIRONEMENT == "dev") console.log(game)
+					<Button
+						title={"goto dash"}
+						onPress={() => navigation.navigate("Dashboard")}
+					/>
+					<View
+						style={styles.separator} //forandroid manly
+					></View>
 
-        if (game && game.logoUrl && validURL(game.logoUrl))
-            setlogoUrl(game.logoUrl);
-    }, [game])
-
-    const renderBulletList = ({ item }: any) => <View style={styles.row}>
-        <View style={styles.bulletContainer}>
-            <View style={styles.bullet}>
-                <Text>{'\u2022' + " "}</Text>
-            </View>
-            <Text style={styles.bulletSubText}>{item.name}</Text>
-        </View>
-    </View>
-        ;
-
-    return (
-        <View>
-            <ProtectedHeader back={"Dashboard"} navigation={navigation} />
-            <ViewContainer>
-                <SmallLineBreak />
-                <TextSubTitle style={styles.titleGame}>Info</TextSubTitle>
-                <ScrollView>
-
-                    <View style={styles.textToMiddle}>
-
-                        {game ? <TextTitle>{game?.name}</TextTitle> : undefined}
-                        <SmallLineBreak />
-
-                        <Image
-                            style={styles.gameLogo}
-                            source={{
-                                uri: logoUrl,
-                            }}
-                        />
-                        <SmallLineBreak />
-
-                        <TextSubTitle>Compétitions : </TextSubTitle>
-                        <SmallLineBreak />
-
-                        <View style={styles.column}>
-                            <FlatList data={game?.leagueIdList} renderItem={renderBulletList} keyExtractor={(x) => x.id} />
-                        </View>
-
-                        <SmallLineBreak />
-                        <TextSubTitle>Code du Contest : </TextSubTitle>
-                        <SmallLineBreak />
-
-                    </View>
-                </ScrollView>
-
-
-                <Button title={"goto dash"} onPress={() => navigation.navigate("Dashboard")} />
-            </ViewContainer>
-        </View>
-    );
+				</ScrollView>
+			</ViewContainer>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    titleGame: {
-        fontSize: 22,
-        fontWeight: "500",
-
-    }, gameLogo: {
-        width: 100,
-        height: 100,
-        // display: "block",
-        marginLeft: "auto",
-        marginRight: "auto",
-    },
-    column: {
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        width: 200
-    },
-    row: {
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        flexWrap: 'wrap',
-        flex: 1
-    },
-    bullet: {
-        width: 10
-    },
-    bulletContainer: {
-        flexDirection: 'row',
-    }, bulletSubText: { color: "black" },
-    boldText: {
-        fontWeight: 'bold'
-    }, textToMiddle: {
-        alignItems: "center",
-    }
+	titleGame: {
+		fontSize: 22,
+		fontWeight: "500",
+	},
+	gameLogo: {
+		width: 100,
+		height: 100,
+		// display: "block",
+		marginLeft: "auto",
+		marginRight: "auto",
+	},
+	column: {
+		flexDirection: "column",
+		alignItems: "flex-start",
+		width: 200,
+	},
+	row: {
+		flexDirection: "row",
+		alignItems: "flex-start",
+		flexWrap: "wrap",
+		flex: 1,
+	},
+	bullet: {
+		width: 10,
+	},
+	bulletContainer: {
+		flexDirection: "row",
+	},
+	bulletSubText: { color: "black" },
+	boldText: {
+		fontWeight: "bold",
+	},
+	textToMiddle: {
+		alignItems: "center",
+	},
+	separator: {
+		marginVertical: 100,
+		height: 1,
+		width: "80%",
+	},
 });
