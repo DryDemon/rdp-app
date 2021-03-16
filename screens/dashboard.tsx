f0b37bb989ee54ffaf9cae677ffcfd56b4954bba
jean chartier-kastler <jean.chartierkastler@gmail.com>
2021-03-16 19:05:14 +0100

    test pour antoine

--- screens/dashboard.tsx
+++ screens/dashboard.tsx
@@ -77,8 +77,8 @@ export default function Dashboard({ navigation }: any) {
 
 	useEffect(() => {
 		if (ENVIRONEMENT == "dev") {
-			AsyncStorage.setItem("@joinCode", "CFEVPU");
-			navigation.navigate("Game")
+			// AsyncStorage.setItem("@joinCode", "CFEVPU");
+			// navigation.navigate("Game")
 		}
 	}, [games]);
 	
