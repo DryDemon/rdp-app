1d1b6761784d6b65678cab888b0c31533f248a1f
AntoineBaudot git config --global user.name "AntoineBaudot" git config --global user.name AntoineBaudot <baudotab@gmail.com>
2021-04-01 12:14:27 +0200

    update colors constants

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,8 +1,12 @@
 export default {
   text: '#000',
   background: '#E5E5E5',
-  rdpColor: '#EF8032',
+  rdpColor: '#EB472D',
   revertRdpColor: "#E2F1FF",
+  white : "#ffffff",
+  blue : "#e2f1ff",
+  orange: '#EB472D',
+  black : "#414141",
 
   tint: '#2f95dc',
   tabIconDefault: '#ccc',
