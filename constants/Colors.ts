56b2a5e7caf6882da89e9293afea9d057ea34f86
jean chartier-kastler <jean.chartierkastler@gmail.com>
2021-04-06 20:25:38 +0200

    tab

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,11 +1,11 @@
 export default {
-  text: '#000',
-  background: '#E5E5E5',
-  rdpColor: '#EB472D',
-  revertRdpColor: "#E2F1FF",
-  white : "#ffffff",
-  blue : "#e2f1ff",
-  orange: '#EB472D',
-  black : "#414141",
-  grayPlaceHolder:"#A9AAB0"
+	text: "#000",
+	background: "#E5E5E5",
+	rdpColor: "#EB472D",
+	revertRdpColor: "#E2F1FF",
+	white: "#ffffff",
+	blue: "#e2f1ff",
+	orange: "#EB472D",
+	black: "#414141",
+	grayPlaceHolder: "#A9AAB0",
 };
