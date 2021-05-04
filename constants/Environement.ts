2bd824b2c4c1f16cc73105cb0b67c4276de7f551
Jean Chartier-Kastler <jean.chartierkastler@gmail.com>
2021-05-05 01:14:59 +0200

    modif constante de reload des paris live

--- constants/Environement.ts
+++ constants/Environement.ts
@@ -2,4 +2,4 @@ export const ENVIRONEMENT: "dev"|"prod" = "prod";
 
 export const CONST_BASE_MISE_PARI = 100;
 
-export const RELOAD_LIVE_BETS_EVERY_SECONDS = 10;
+export const RELOAD_LIVE_BETS_EVERY_SECONDS = 15;
