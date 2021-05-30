e74f46a7df2a4256591b511dbd62c629a237bc62
Jean.Chartier <jean.chartier-kastler_ext@michelin.com>
2021-05-30 22:50:23 +0200

    correction des icones

--- components/CustomIcon.tsx
+++ components/CustomIcon.tsx
@@ -1,24 +1,45 @@
 import React from "react";
- 
+
 import IcomoonReact from "icomoon-react";
 import iconSet from "../src/selection.json";
- 
-const Icon: React.FC<{
-  color?: string,
-  size: string | number,
-  icon: string,
-  className?: string 
-}> = props => {
-  const { color, size = "100%", icon, className = "" } = props;
-  return (
-    <IcomoonReact
-      className={className}
-      iconSet={iconSet}
-      color={color}
-      size={size}
-      icon={icon}
-    />
-  );
-};
- 
-export default Icon;
+import { View, Text } from "./Themed";
+
+import selection from "../src/selection.json";
+
+export function getUnicode(icon: string) {
+	let code = selection.icons.find((value) => {
+		return value.properties.name == icon;
+	});
+
+	if (code) return String.fromCodePoint(code.properties.code);
+
+	return "";
+}
+
+export default function Icon(props: any) {
+	let { color, size = "100%", icon, className = "" } = props;
+	let realSize = 24;
+
+	if (size && typeof size == "string" && size.indexOf("%") !== -1) {
+		size = size.replace("%", "");
+
+		size = +size;
+		realSize = (24 * size) / 100;
+	}
+
+	if (size && typeof size == "number") realSize = size;
+
+	return (
+		<View>
+			<Text
+				style={{
+					color: color,
+					fontFamily: "icomoon",
+					fontSize: realSize,
+				}}
+			>
+				{getUnicode(icon)}
+			</Text>
+		</View>
+	);
+}
